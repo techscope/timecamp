188153463e036864bdb88ead364b56c51a40c5cb
TechScope LLC <christian.soseman@techscopellc.com>
2017-04-03 14:20:47 +0000

    Initial commit

---
+++ src/TimecampServiceProvider.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace techscope\Timecamp;
+
+use Illuminate\Support\ServiceProvider;
+
+class TimecampServiceProvider extends ServiceProvider
+{
+    /**
+     * Perform post-registration booting of services.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+        //
+    }
+
+    /**
+     * Register any package services.
+     *
+     * @return void
+     */
+    public function register()
+    {
+        //
+    }
+}
