cd18dbfa8dee84d20d3d021fb968dddbf3ddcb9d
TechScope LLC <christian.soseman@techscopellc.com>
2017-06-10 16:59:15 +0000

    Adds get, update, add methods to TimeEntry model

--- src/models/TimeEntryModel.php
+++ src/models/TimeEntryModel.php
@@ -9,8 +9,9 @@
 namespace Techscope\Timecamp;
 
 
+use Carbon\Carbon;
 use Faker\Provider\Base;
-
+// https://github.com/timecamp/timecamp-api/blob/master/sections/time-entries.md
 class TimeEntryModel extends BaseModel
 {
     protected $fields = [
@@ -29,20 +30,48 @@ class TimeEntryModel extends BaseModel
         "invoiceId"
     ];
 
-    public function get()
+    public function get($query_params = null)
     {
+        if(!isset($query_params['to']))
+        {
+            $query_params['to'] = Carbon::today()->toDateString();
+        }
 
+        if(!isset($query_params['from']))
+        {
+            $query_params['from'] = Carbon::today()->subWeeks(2)->toDateString();
+        }
+
+        $http_get_string = http_build_query($query_params);
+        $request = $this->guzzle->request('GET', "entries/{$this->url_tail}?$http_get_string");
+
+        $response = $this->getResponseAsArray($request);
+        return $response;
     }
 
-    public function update()
+    public function update($entry_id, array $parameters)
     {
+        $parameters['id'] = $entry_id;
+        $request = $this->guzzle->request('PUT', "entries/{$this->url_tail}", [
+            'form_params' => $parameters
+        ]);
 
+        $response = $this->getResponseAsArray($request);
+        return $response;
     }
 
-    public function create()
+    public function add($name, array $parameters = [])
     {
+        $parameters['name'] = $name;
 
+        $request = $this->guzzle->request('POST', "tasks/{$this->url_tail}", [
+            'form_params' => $parameters
+        ]);
+
+        $response = $this->getResponseAsArray($request);
+        return $response;
     }
+
 
     // See https://github.com/timecamp/timecamp-api/blob/master/sections/time-entries.md GET /entries_changes
     public function getChanges()
