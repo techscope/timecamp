9d6d823e7813aca7ddc4c242b39be1aeef1b9a94
TechScope LLC <christian.soseman@techscopellc.com>
2017-06-10 04:01:02 +0000

    Adds get, update, and add methods to TaskModel

--- src/models/TaskModel.php
+++ src/models/TaskModel.php
@@ -30,4 +30,54 @@ class TaskModel extends BaseModel
         "isValid",
         "isFilled"
     ];
+
+    // TODO: Talk to Kamil; the note field should really be shown here
+    public function get($task_id = null)
+    {
+        if(!is_null($task_id))
+        {
+            $request = $this->guzzle->request('GET', "tasks/{$this->url_tail}?task_id=$task_id");
+        } else {
+            $request = $this->guzzle->request('GET', "tasks/{$this->url_tail}");
+        }
+
+        $response = $this->getResponseAsArray($request);
+        return $response;
+    }
+
+    public function update($task_id, array $parameters)
+    {
+        $parameters['task_id'] = $task_id;
+        $request = $this->guzzle->request('PUT', "tasks/{$this->url_tail}", [
+            'form_params' => $parameters
+        ]);
+
+        $response = $this->getResponseAsArray($request);
+        return $response;
+    }
+
+    public function add($name, array $parameters = [])
+    {
+        $parameters['name'] = $name;
+
+        $request = $this->guzzle->request('POST', "tasks/{$this->url_tail}", [
+            'form_params' => $parameters
+        ]);
+
+        $response = $this->getResponseAsArray($request);
+        return $response;
+    }
+
+    // TODO: DELETE function not working. Find out if it is offered in the API
+//    public function delete($task_id)
+//    {
+//        $request = $this->guzzle->request('DELETE', "tasks/{$this->url_tail}", [
+//            'form_params' => [
+//                "id" => $task_id
+//            ]
+//        ]);
+//
+//        $response = $this->getResponseAsArray($request);
+//        return $response;
+//    }
 }
