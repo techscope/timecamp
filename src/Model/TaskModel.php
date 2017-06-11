195402878bd05c5f6ac84c56aef379454e6c43d8
TechScope LLC <christian.soseman@techscopellc.com>
2017-06-11 22:18:51 +0000

    Adds returned fields; adjusts the way GET and UPDATE return data (strips the associative array key for record number)

--- src/Model/TaskModel.php
+++ src/Model/TaskModel.php
@@ -14,21 +14,31 @@ use Faker\Provider\Base;
 class TaskModel extends BaseModel
 {
     protected $fields = [
-        "api_token",
-        "api_token",
-        "id",
-        "user_id",
-        "date",
-        "task_id",
-        "duration",
-        "start_time",
-        "end_time",
-        "description",
-        "billable",
-        "invoiceId",
-        "approval_id",
-        "isValid",
-        "isFilled"
+        "task_id" => ["RetGet", "RetAdd", "RetUpdate"],
+        "parent_id" => ["RetGet", "RetAdd", "RetUpdate"],
+        "assigned_by" => ["RetGet", "RetAdd", "RetUpdate"],
+        "assigned_to" => ["RetAdd", "RetUpdate"],
+        "name" => ["RetGet", "RetAdd", "RetUpdate"],
+        "external_task_id" => ["RetGet", "RetAdd", "RetUpdate"],
+        "external_parent_id" => ["RetGet", "RetAdd", "RetUpdate"],
+        "level" => ["RetGet", "RetAdd", "RetUpdate"],
+        "add_date" => ["RetAdd", "RetUpdate"],
+        "archived" => ["RetGet", "RetAdd", "RetUpdate"],
+        "color" => ["RetGet", "RetAdd", "RetUpdate"],
+        "tags" => ["RetGet", "RetAdd", "RetUpdate"],
+        "budgeted" => ["RetGet", "RetAdd", "RetUpdate"],
+        "checked_date" => ["RetAdd", "RetUpdate"],
+        "budget_unit" => ["RetGet", "RetAdd", "RetUpdate"],
+        "due_date" => ["RetAdd", "RetUpdate"],
+        "note" => ["RetAdd", "RetUpdate"],
+        "context" => ["RetAdd", "RetUpdate"],
+        "folder" => ["RetAdd", "RetUpdate"],
+        "repeat" => ["RetAdd", "RetUpdate"],
+        "root_group_id" => ["RetGet", "RetAdd", "RetUpdate"],
+        "billable" => ["RetGet", "RetAdd", "RetUpdate"],
+        "user_access_type" => ["RetGet"]
+        // TODO: Figure out a good way to handle the returned user array in GET
+        // TODO: By default, an associated array is returned for all methods
     ];
 
     // TODO: Talk to Kamil; the note field should really be shown here
@@ -42,7 +52,10 @@ class TaskModel extends BaseModel
         }
 
         $response = $this->getResponseAsArray($request);
-        return $response;
+        // For some reason these results are returned as an associative array with the key being an integer. This means
+        // we can just use $response[0] to get the first value. To make this more standardize we are going to use
+        // array_values() to strip the non-sequention numeric keys the API returns.
+        return array_values($response);
     }
 
     public function update($task_id, array $parameters)
@@ -65,7 +78,7 @@ class TaskModel extends BaseModel
         ]);
 
         $response = $this->getResponseAsArray($request);
-        return $response;
+        return array_values($response)[0];
     }
 
     // TODO: DELETE function not working. Find out if it is offered in the API
