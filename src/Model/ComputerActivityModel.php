50429fbe6973c7d2ffb7e6712be438dce77b527c
TechScope LLC <christian.soseman@techscopellc.com>
2017-06-12 06:04:22 +0000

    ComputerActivityModel: Adds getWindow and add methods; adds returned field values

--- src/Model/ComputerActivityModel.php
+++ src/Model/ComputerActivityModel.php
@@ -12,11 +12,17 @@ class ComputerActivityModel extends BaseModel
 {
     protected $fields = [
         "user_id" => ["RetGet"],
-        "user_name" => ["RetGet"],
-        "start_time" => ["RetGet"],
+        "application_id" => ["RetGet", "RetGetApp"],
         "end_time" => ["RetGet"],
-        "total_time" => ["RetGet"],
-        "date" => ["RetGet"]
+        "time_span" => ["RetGet"],
+        "window_title_id" => ["RetGet"],
+        "end_date" => ["RetGet"],
+        "task_id" => ["RetGet"],
+        "entry_id" => ["RetGet"],
+        "app_name" => ["RetGetApp"],
+        "aditional_info" => ["RetGetApp"],
+        "full_name" => ["RetGetApp"],
+        "category_id" => ["RetGetApp"]
         // TODO: can't figure out how to populate date in these fields, talk with Kamil to see what triggers data to show up here
     ];
 
@@ -37,8 +43,18 @@ class ComputerActivityModel extends BaseModel
         return $response;
     }
 
-    public function add($parameters = null)
+    public function add($application_name, $start_datetime, $end_datetime, $window_title = null, $website_domain = null)
     {
+        $parameters['application_name'] = $application_name;
+        $parameters['start_time'] = $start_datetime;
+        $parameters['end_time'] = $end_datetime;
+
+        if(!is_null($window_title))
+            $parameters['window_title'] = $window_title;
+
+        if(!is_null($website_domain))
+            $parameters['website_domain'] = $website_domain;
+
         $request = $this->guzzle->request('POST', "activity/{$this->url_tail}", [
             'form_params' => $parameters
         ]);
@@ -56,6 +72,20 @@ class ComputerActivityModel extends BaseModel
 
         $response = $this->getResponseAsArray($request);
 
-        return $response;
+        return array_values($response);
     }
+
+    public function getWindow($window_ids)
+    {
+        $query_params['window_ids'] = $window_ids;
+
+        $http_get_string = http_build_query($query_params);
+        $request = $this->guzzle->request('GET', "window_title/{$this->url_tail}?$http_get_string");
+
+        $response = $this->getResponseAsArray($request);
+
+        return array_values($response);
+    }
+
+
 }
